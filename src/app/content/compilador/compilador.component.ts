9a3ba6a1c1181a2b0f70cf80fa53898e012201d5
Nery Orellana <norellanac@10x.org>
2020-05-23 14:47:10 -0600

    array de cada palabra del texto, agrupado por linea, y funcion para encontrar todas las declaraciones

--- src/app/content/compilador/compilador.component.ts
+++ src/app/content/compilador/compilador.component.ts
@@ -1,31 +1,83 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
-import { ThrowStmt } from '@angular/compiler';
-import { CompierrorComponent } from './compierror/compierror.component';
-import { ObjectUnsubscribedError } from 'rxjs';
-
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { MatDialog } from "@angular/material/dialog";
+import { ThrowStmt } from "@angular/compiler";
+import { CompierrorComponent } from "./compierror/compierror.component";
+import { ObjectUnsubscribedError } from "rxjs";
 
 const ELEMENT_DATA: any[] = [
-  { id: 1, Token: ' = ', Categoria: 'Igualdador', Tipo: 'Igualador', Valor: '-', Prioridad: '1' },
-  { id: 1, Token: ' Temp1 ', Categoria: 'Variable', Tipo: 'Variable', Valor: '-', Prioridad: '-' },
-  { id: 1, Token: ' Temp1 ', Categoria: 'Variable', Tipo: 'Variable', Valor: '-', Prioridad: '-' },
-  { id: 1, Token: ' Temp1 ', Categoria: 'Variable', Tipo: 'Variable', Valor: '-', Prioridad: '-' },
-  { id: 1, Token: ' Temp1 ', Categoria: 'Variable', Tipo: 'Variable', Valor: '-', Prioridad: '-' },
-  { id: 1, Token: ' Temp1 ', Categoria: 'Variable', Tipo: 'Variable', Valor: '-', Prioridad: '-' },
-  { id: 1, Token: ' Temp1 ', Categoria: 'Variable', Tipo: 'Variable', Valor: '-', Prioridad: '-' },
+  {
+    id: 1,
+    Token: " = ",
+    Categoria: "Igualdador",
+    Tipo: "Igualador",
+    Valor: "-",
+    Prioridad: "1",
+  },
+  {
+    id: 1,
+    Token: " Temp1 ",
+    Categoria: "Variable",
+    Tipo: "Variable",
+    Valor: "-",
+    Prioridad: "-",
+  },
+  {
+    id: 1,
+    Token: " Temp1 ",
+    Categoria: "Variable",
+    Tipo: "Variable",
+    Valor: "-",
+    Prioridad: "-",
+  },
+  {
+    id: 1,
+    Token: " Temp1 ",
+    Categoria: "Variable",
+    Tipo: "Variable",
+    Valor: "-",
+    Prioridad: "-",
+  },
+  {
+    id: 1,
+    Token: " Temp1 ",
+    Categoria: "Variable",
+    Tipo: "Variable",
+    Valor: "-",
+    Prioridad: "-",
+  },
+  {
+    id: 1,
+    Token: " Temp1 ",
+    Categoria: "Variable",
+    Tipo: "Variable",
+    Valor: "-",
+    Prioridad: "-",
+  },
+  {
+    id: 1,
+    Token: " Temp1 ",
+    Categoria: "Variable",
+    Tipo: "Variable",
+    Valor: "-",
+    Prioridad: "-",
+  },
 ];
 
-
-
 @Component({
-  selector: 'app-compilador',
-  templateUrl: './compilador.component.html',
-  styleUrls: ['./compilador.component.scss']
+  selector: "app-compilador",
+  templateUrl: "./compilador.component.html",
+  styleUrls: ["./compilador.component.scss"],
 })
 export class CompiladorComponent implements OnInit {
-
-  displayedColumns: string[] = ['id', 'Token', 'Categoria', 'Tipo', 'Valor', 'Prioridad'];
+  displayedColumns: string[] = [
+    "id",
+    "Token",
+    "Categoria",
+    "Tipo",
+    "Valor",
+    "Prioridad",
+  ];
   dataSource = ELEMENT_DATA;
 
   form: FormGroup;
@@ -33,31 +85,59 @@ export class CompiladorComponent implements OnInit {
   totalLines = 0;
   totalWords = 0;
   totalRepeated;
+  textArraySpacesGroupLines = [];
+  declarationTokens = [{
+    token: undefined,
+    category: undefined,
+    type: undefined,
+    valToken: undefined,
+    priority: undefined
+  }];
 
-  dataValue = '';
+  dataValue = "";
 
   erroList = [];
 
-  constructor(
-    public matDialog: MatDialog
-  ) { }
+  constructor(public matDialog: MatDialog) {}
 
   ngOnInit() {
-
     this.form = new FormGroup({
-      input: new FormControl(this.dataValue)
+      input: new FormControl(this.dataValue),
     });
-
   }
 
-  onSubmit() {
+  onSubmit() {}
 
+  //obtiene todo el texto lo separa por saltos de linea y cada linea es un objeto por espacios
+  textToarrayTable(text) {
+    //cada linea se vuelve un array
+    var textLines = [];
+    textLines = text.split("\n");
+    //se crea un array por cada linea, y el array contiene las separaciones por espacio
+    for (var i = 0; i < textLines.length; i++) {
+      var lineArray = textLines[i].toString().split(" "); // separa por espacios
+      this.textArraySpacesGroupLines.push(lineArray); //añade el array por espacio al array de lineas
+    }
+    console.log("=====spread===============================");
+    console.log(
+      "Array de spacios agrupado por lineas: ",
+      this.textArraySpacesGroupLines
+    );
+    console.log("====================================");
+  }
+
+  dataGeneralTable(arrayLineSpaces){
+    for (const item of arrayLineSpaces) {
+      if (item.includes("As")){
+        console.log('decalración: ', item);
+      }
+      
+    }
   }
 
   processData() {
-
     this.erroList = [];
-    const textToProcess = this.form.get('input').value;
+    const textToProcess = this.form.get("input").value;
 
     // PASO 1, PASAR EL TEXTO A LINEAS
     let listOfLines = this.getLinesInArray(textToProcess);
@@ -78,11 +158,15 @@ export class CompiladorComponent implements OnInit {
     if (this.erroList.length > 0) {
       this.callError(listOfLines);
     }
+
+    //
+    this.textToarrayTable(textToProcess);
+    this.dataGeneralTable(this.textArraySpacesGroupLines);
   }
 
   getLinesInArray(text: any) {
     const tempOne = text;
-    const tempSec = tempOne.split('\n');
+    const tempSec = tempOne.split("\n");
     return tempSec;
   }
 
@@ -90,9 +174,8 @@ export class CompiladorComponent implements OnInit {
     const newTempList = [];
 
     if (list.length > 0) {
-
-      list.forEach(obj => {
-        if (obj !== '') {
+      list.forEach((obj) => {
+        if (obj !== "") {
           newTempList.push(obj.trim());
         }
       });
@@ -100,25 +183,21 @@ export class CompiladorComponent implements OnInit {
       // newTempList.forEach( (obj: string, index: number) => {
       //   newTempList[index] = obj.replace(/\s/g, '');
       // });
-
     }
 
     return newTempList;
   }
 
   checkIfSemiColon(lineTocheck: string[]) {
-
     if (lineTocheck.length > 0) {
-
       let indexIs = 0;
-      lineTocheck.forEach(obj => {
-
-        let receivedString = '';
+      lineTocheck.forEach((obj) => {
+        let receivedString = "";
         receivedString = obj.toString();
         const response = receivedString.charAt(receivedString.length - 1);
 
-        if (response !== ';') {
-          this.erroList.push({ index: indexIs, errorType: 'Semicolon' });
+        if (response !== ";") {
+          this.erroList.push({ index: indexIs, errorType: "Semicolon" });
         }
 
         indexIs++;
@@ -127,32 +206,27 @@ export class CompiladorComponent implements OnInit {
   }
 
   checkParentesis(linesToCheck: string[]) {
-
     const tempList = linesToCheck;
 
     if (tempList.length > 0) {
-
       tempList.forEach((obj: string, index: number) => {
-
         if (this.checkIfIsFormula(obj)) {
           this.findParentesis(obj, index);
         }
       });
-
     }
 
     return tempList;
   }
 
   findParentesis(wordList: string, indexIs: number) {
-
     let totalParentesisApertura = 0;
     let totalParentesisCierre = 0;
 
-    const tempOne = wordList.split('(');
+    const tempOne = wordList.split("(");
     totalParentesisApertura = tempOne.length;
 
-    const tempSec = wordList.split(')');
+    const tempSec = wordList.split(")");
     totalParentesisCierre = tempSec.length;
 
     const totalOpen = totalParentesisApertura - 1;
@@ -161,143 +235,137 @@ export class CompiladorComponent implements OnInit {
     if (totalOpen !== totalClose) {
       this.erroList.push({
         index: indexIs,
-        errorType: 'parentesis',
-        message: ' Parentesis Abiertos ' + totalOpen + ' Parentesis Cerrados ' + totalClose,
+        errorType: "parentesis",
+        message:
+          " Parentesis Abiertos " +
+          totalOpen +
+          " Parentesis Cerrados " +
+          totalClose,
       });
     }
-
   }
 
   callError(listOfLines: any) {
     this.matDialog.open(CompierrorComponent, {
-      width: '700px',
+      width: "700px",
       data: {
         list: listOfLines,
-        errorList: this.erroList
-      }
+        errorList: this.erroList,
+      },
     });
   }
 
   checkFormulaStructure(listOfLines: any[]) {
     if (listOfLines.length > 0) {
-
       listOfLines.forEach((obj: string, index: number) => {
+        const spitFormula = obj.split("=");
 
-        const spitFormula = obj.split('=');
+        if (this.checkIfIsFormula(obj)) {
+          if (obj.includes("+")) {
+            this.splitAndCheck("+", spitFormula[1], index);
+          }
 
-        if ( this.checkIfIsFormula(obj) ) {
+          if (obj.includes("-")) {
+            this.splitAndCheck("-", spitFormula[1], index);
+          }
 
-              if ( obj.includes('+') ) {
-                this.splitAndCheck('+', spitFormula[1], index);
-              }
+          if (obj.includes("/")) {
+            this.splitAndCheck("/", spitFormula[1], index);
+          }
 
-              if ( obj.includes('-') ) {
-                this.splitAndCheck('-', spitFormula[1], index);
-              }
-
-              if ( obj.includes('/') ) {
-                this.splitAndCheck('/', spitFormula[1], index);
-              }
-
-              if ( obj.includes('*') ) {
-                this.splitAndCheck('*', spitFormula[1], index);
-              }
-
+          if (obj.includes("*")) {
+            this.splitAndCheck("*", spitFormula[1], index);
+          }
         }
-
       });
-
     }
   }
 
   splitAndCheck(sign: string, textToSplit: string, indexIs: number) {
-
-    console.log('split by', sign);
+    console.log("split by", sign);
     // quitar espacions en blanco
-    const textToWork = textToSplit.replace(/\s/g, '');
+    const textToWork = textToSplit.replace(/\s/g, "");
     // split by simbolo
     const tempArray = textToWork.split(sign);
     // arreglo
-    console.log('tempArray is', tempArray);
+    console.log("tempArray is", tempArray);
 
-    tempArray.forEach(obj => {
-
+    tempArray.forEach((obj) => {
       if (obj.length > 1) {
-
-              // caracter inicial
-              if (
-                obj[0] === '+' ||
-                obj[0] === '-' ||
-                obj[0] === '/' ||
-                obj[0] === '*' ||
-                obj[0] === '' ||
-                obj[0] === ')'
-                 ) {
-
-                this.erroList.push({
-                  index: indexIs,
-                  errorType: 'structure',
-                  message: 'Error de estructura - cerca de simbolo >> ' + sign,
-                });
-
-              }
-
-              // carater final
-              if (
-                obj[obj.length - 1] === '+' ||
-                obj[obj.length - 1] === '-' ||
-                obj[obj.length - 1] === '/' ||
-                obj[obj.length - 1] === '*' ||
-                obj[obj.length - 1] === ''  ||
-                obj[obj.length - 1] === '('
-                ) {
-                this.erroList.push({
-                  index: indexIs,
-                  errorType: 'structure',
-                  message: '*Error de estructura - cerca de simbolo >>> ' + sign,
-                });
-              }
-
-      } else  if (obj.length === 1 ) {
-              // solo es un caracter
-              if (obj === '+' || obj === '-' || obj === '/' || obj === '*' || obj === '' || obj === ';' ) {
-                this.erroList.push({
-                  index: indexIs,
-                  errorType: 'structure',
-                  message: 'Error de Estructura cerca de simbolo > ' + sign,
-                });
-              }
-      } else  if (obj.length === 0 ) {
-
-        this.erroList.push({
+        // caracter inicial
+        if (
+          obj[0] === "+" ||
+          obj[0] === "-" ||
+          obj[0] === "/" ||
+          obj[0] === "*" ||
+          obj[0] === "" ||
+          obj[0] === ")"
+        ) {
+          this.erroList.push({
             index: indexIs,
-            errorType: 'structure',
-            message: 'Error de Estructura cerca de simbolo > ' + sign,
+            errorType: "structure",
+            message: "Error de estructura - cerca de simbolo >> " + sign,
           });
-}
+        }
 
+        // carater final
+        if (
+          obj[obj.length - 1] === "+" ||
+          obj[obj.length - 1] === "-" ||
+          obj[obj.length - 1] === "/" ||
+          obj[obj.length - 1] === "*" ||
+          obj[obj.length - 1] === "" ||
+          obj[obj.length - 1] === "("
+        ) {
+          this.erroList.push({
+            index: indexIs,
+            errorType: "structure",
+            message: "*Error de estructura - cerca de simbolo >>> " + sign,
+          });
+        }
+      } else if (obj.length === 1) {
+        // solo es un caracter
+        if (
+          obj === "+" ||
+          obj === "-" ||
+          obj === "/" ||
+          obj === "*" ||
+          obj === "" ||
+          obj === ";"
+        ) {
+          this.erroList.push({
+            index: indexIs,
+            errorType: "structure",
+            message: "Error de Estructura cerca de simbolo > " + sign,
+          });
+        }
+      } else if (obj.length === 0) {
+        this.erroList.push({
+          index: indexIs,
+          errorType: "structure",
+          message: "Error de Estructura cerca de simbolo > " + sign,
+        });
+      }
     });
 
-    if ( textToWork.length <= 3 ) {
-
+    if (textToWork.length <= 3) {
       this.erroList.push({
         index: indexIs,
-        errorType: 'structure',
-        message: 'Error de Estructura cerca de simbolo, debe tener al menos 3 datos para poder procesar',
+        errorType: "structure",
+        message:
+          "Error de Estructura cerca de simbolo, debe tener al menos 3 datos para poder procesar",
       });
-
     }
-
   }
 
   checkIfIsFormula(objToCheck: string) {
     if (
-      objToCheck.toLowerCase().includes('+') ||
-      objToCheck.toLowerCase().includes('-') ||
-      objToCheck.toLowerCase().includes('/') ||
-      objToCheck.toLowerCase().includes('*') ||
-      objToCheck.toLowerCase().includes(')') ||
-      objToCheck.toLowerCase().includes('(')
+      objToCheck.toLowerCase().includes("+") ||
+      objToCheck.toLowerCase().includes("-") ||
+      objToCheck.toLowerCase().includes("/") ||
+      objToCheck.toLowerCase().includes("*") ||
+      objToCheck.toLowerCase().includes(")") ||
+      objToCheck.toLowerCase().includes("(")
     ) {
       return true;
     } else {
@@ -306,16 +374,13 @@ export class CompiladorComponent implements OnInit {
   }
 
   showFile(e: any): any {
-
     e.preventDefault();
     const reader = new FileReader();
 
     reader.onload = async (xe: any) => {
-      this.form.get('input').setValue(xe.target.result);
+      this.form.get("input").setValue(xe.target.result);
     };
 
     reader.readAsText(e.target.files[0]);
-
   }
-
 }
